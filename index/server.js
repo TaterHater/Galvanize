16ccf446cbaf92a2f73048ea4348dc66379d56db
huntinator7 <harnell@mines.edu>
2017-09-23 11:05:48 -0600

    server.js using API

--- index/server.js
+++ index/server.js
@@ -2,12 +2,15 @@
 var http = require('http');
 var express = require('express');
 var mysql = require('mysql');
+var messages = require('../routes/messages');
 
 var app = express();
 
 var httpServer = http.createServer(app).listen(80, function () {
     console.log('http redirect server up and running at port 80');
 });
+
+app.use('/messages', messages);
 
 app.post('/', function (req, res) {
     res.send('POST request to the homepage')
