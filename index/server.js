76c51a67d9d33c7585a5b65e536b82c2936ea946
Nick B <nick_beeson@yahoo.com>
2017-09-22 19:57:49 -0600

    added basic socket/express shit

--- index/server.js
+++ index/server.js
@@ -1,1 +1,19 @@
 // JavaScript source code
+var http = require('http');
+var express = require('express');
+var mysql = require('mysql');
+
+var app = express();
+
+var httpServer = http.createServer(app2).listen(80, function () {
+    console.log('http redirect server up and running at port 80');
+});
+
+
+var io = require('socket.io')(httpServer);
+
+
+io.sockets.on('connection', function (socket) {
+
+
+}
