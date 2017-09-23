3d3ee0ed935e77864c97f895e7954c0f75d2270c
TaterHater <nick_beeson@yahoo.com>
2017-09-23 11:23:14 -0600

    updated reference

--- routes/messages.js
+++ routes/messages.js
@@ -24,7 +24,28 @@ router.get('/risks', function (req, res, next) {
         });
     }
 });
-
+router.get('/sites', function (req, res, next) {
+    if (req.params.id) {
+        Mess.GetRisk(function (err, rows) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(rows);
+            }
+        });
+    }
+});
+router.get('/getform/:site?', function (req, res, next) {
+    if (req.params.id) {
+        Mess.GetformBySite(req.params.site, function (err, rows) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(rows);
+            }
+        });
+    }
+});
 
 
 module.exports = router;
