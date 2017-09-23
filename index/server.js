975ce5309781862e06dfe1a44e38d1f70b0e1cf8
Nick B <nick_beeson@yahoo.com>
2017-09-23 13:45:16 -0600

    testig loadRisks

--- index/server.js
+++ index/server.js
@@ -23,29 +23,31 @@ app.get('/home', function (req, res) {
     res.sendFile(path.resolve(__dirname + "/../public/html/home.html"));
 });
 
-
+//mysql connection
 var connect = {
     host: "localhost",
     user: "root",
     password: "raspberry",
     database: "mooserisk"
 };
+var con = mysql.createConnection(connect); 
 
+//adds user to database
 function addUser(name, email, isAdmin) {
     con.query("insert into users (name,email,is_admin) values (?,?,0,?) ", [name, email, isAdmin]);
-
 }
 function addForm(file) {
     //parse file, then add to db
 }
 function loadRisks() {
     var content = fs.readFileSync("http://www.moosen.im/messages/risks");
-
+    console.log(content);
     //site cna have mine, processing plant, offices.
     // add location to user, and indiviual sites are the only thing we need to update. 
     //add image uploads to forms
     //randomize question order to keep users on their toes. 
 };
+loadRisks();
 
-var con = mysql.createConnection(connect); 
+
 
