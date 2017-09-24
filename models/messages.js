02b4e4919e3bb7a6ab5921250a6be8e9af1e9821
TaterHater <nick_beeson@yahoo.com>
2017-09-24 00:07:03 -0600

    leaderboard works!

--- models/messages.js
+++ models/messages.js
@@ -23,7 +23,7 @@ var messages = {
         return con.query("SELECT * From form where site = ?", [site], callback);
     },
     getLeaderboard: function (callback) {
-        return con.query("SELECT * FROM ( SELECT * FROM users ORDER BY score DESC LIMIT 10) sub ORDER BY  score ASC", callback);
+        return con.query("SELECT * FROM ( SELECT * FROM users ORDER BY score DESC LIMIT 10) sub ORDER BY  score DESC", callback);
     }
 
 };
